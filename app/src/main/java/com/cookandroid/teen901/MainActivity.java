08a0df1bb30d493de344980164d208977e95c468
CHO HYE RA <s2037@e-mirim.hs.kr>
2021-05-22 01:22:40 +0900

    학교명을 입력하면 맞는지 확인

---
+++ app/src/main/java/com/cookandroid/teen901/MainActivity.java
@@ -0,0 +1,38 @@
+package com.cookandroid.teen901;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email_id;
+    Button email_btn_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // find ViewById
+        email_id = (EditText) findViewById(R.id.email_id);
+        email_btn_id = (Button) findViewById(R.id.email_btn_id);
+
+
+        email_btn_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (email_id.getText().toString().equals("미림여자정보과학고등학교")) {
+                    // 미림여자정보과학고등학교를 입력하였을때 Toast로 "환영합니다"를 출력
+                    Toast.makeText(getApplicationContext(), "환영합니다", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "환영안함", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
