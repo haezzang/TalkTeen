b1edf55f8d8a436643bc0d207eb13effa6a7371b
CHO HYE RA <s2037@e-mirim.hs.kr>
2021-05-23 16:05:21 +0900

    back버튼 추가 및 과자 리니어 중첩 수정해야함

--- app/src/main/java/com/cookandroid/teen901/schoolmain.java
+++ app/src/main/java/com/cookandroid/teen901/schoolmain.java
@@ -1,15 +1,31 @@
 package com.cookandroid.teen901;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class schoolmain extends AppCompatActivity {
+    ImageButton back_btn;
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schoolmain);
         setTitle("TalkTeen");
+
+        // findViewById
+        back_btn = (ImageButton)findViewById(R.id.back_btn);
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // back 화사표 클릭시 로그인 액티비티 전환
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
